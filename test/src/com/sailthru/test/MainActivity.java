34a117459f334f8be67ec0ab1766a37198fb58c9
Parthiban Loganathan <pl2487@columbia.edu>
2013-06-11 10:45:40 -0400

    Add onclick methods

--- test/src/com/sailthru/test/MainActivity.java
+++ test/src/com/sailthru/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.sailthru.test;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
@@ -21,4 +22,13 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void logo(View v)
+    {
+    	Toast.makeText(getApplicationContext(), "Logo pressed", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void whiteButton(View v)
+    {
+    	Toast.makeText(getApplicationContext(), "White button presssed", Toast.LENGTH_SHORT).show();
+    }
 }
