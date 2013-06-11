0070f4acb309ad2b50b0a9243279c84d94d4dd83
Parthiban Loganathan <pl2487@columbia.edu>
2013-06-11 12:30:59 -0400

    Remove unnecessary intent from MyApplication

--- test/src/com/sailthru/test/MyApplication.java
+++ test/src/com/sailthru/test/MyApplication.java
@@ -1,13 +1,11 @@
 package com.sailthru.test;
 
 import android.app.Application;
-import android.content.Intent;
 
 import com.urbanairship.AirshipConfigOptions;
 import com.urbanairship.Logger;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.PushManager;
-import com.urbanairship.push.PushPreferences;
 
 public class MyApplication extends Application {
 
@@ -24,10 +22,5 @@ public class MyApplication extends Application {
 		
 		String apid = PushManager.shared().getAPID();
 		Logger.info("My Application onCreate - App APID: " + apid);
-		
-		
-		/*Intent intent = new Intent(this, MainActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		startActivity(intent);*/
 	}
 }
