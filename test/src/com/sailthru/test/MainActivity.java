c7b378a094f1df3bed68a8203864e3bcd7ed3bfd
Parthiban Loganathan <pl2487@columbia.edu>
2013-06-14 11:45:26 -0400

    Add unit test for queue in MainActivity

--- test/src/com/sailthru/test/MainActivity.java
+++ test/src/com/sailthru/test/MainActivity.java
@@ -1,5 +1,13 @@
 package com.sailthru.test;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import sailthru.Event.STEvent;
+import sailthru.Event.SailthruEvent;
+import sailthru.Exceptions.InvalidSailthruEventException;
+import sailthru.Queuer.STQueuer;
+import sailthru.Utilities.Logger;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -38,5 +46,40 @@ public class MainActivity extends Activity {
     	
         Toast.makeText(getApplicationContext(), "APID: " + apid, Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(), "MID: " + mid, Toast.LENGTH_SHORT).show();
+    }
+    
+    // Queue Unit Test
+    public void queue(View v) throws InvalidSailthruEventException
+    {
+    	// Get elements before queue creation
+    	ConcurrentLinkedQueue<STEvent> queue = STQueuer.getEvents(10);
+    	
+    	if(queue.size() == 0)
+    	{
+    		Logger.i("Before insertion, queue is empty.");
+    	}
+    	
+    	for(int i = 0; i < queue.size(); i++)
+    	{
+    		if(queue.peek() != null)
+    		{
+    			Logger.i("Before insertion, queue contains: " + queue.poll());
+    		}
+    		else
+    		{
+    			Logger.e("Invalid element in queue");
+    		}
+    	}
+    	
+    	// Add elements
+    	ConcurrentLinkedQueue<STEvent> list = new ConcurrentLinkedQueue<STEvent>();
+    	
+    	for(int i = 0; i < 10; i++)
+    	{
+    		SailthruEvent stevent = new SailthruEvent.Builder().tag("t"+i).event("e"+i).build();
+    		list.add(stevent.getSTEvent());
+    	}
+    	
+    	STQueuer.addEvents(list);
     }
 }
